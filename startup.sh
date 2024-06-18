89738cf644d10f3669f19a1c5903f8a4a1ca18e7
Nilaratann <nilaratann@gmail.com>
2024-06-18 18:24:35 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,4 @@ apt-get update
 
 apt-get install -y firefox-esr
 
-pyhton3 manage.py collectstatic --noinput
-
 gunicorn amazon_scrapper.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
