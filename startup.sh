ca15a692f027839356312cef92ccabac552b7cc6
Nilaratan Nayak <nilaratan.nayak2@partsauthority.com>
2024-06-18 17:59:28 +0530

    startup script

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ apt-get update
 
 apt-get install -y firefox-esr
 
-pyhton manage.py collectstatic --noinput
+pyhton3 manage.py collectstatic --noinput
 
 gunicorn amazon_scrapper.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
